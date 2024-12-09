c2e673d871c2069cf280b89d09c160f756db7664
Damon Lewis <damon@thethreethousands.com>
2024-12-09 22:36:04 +1100

    Reverted recent changes to why the Dummy widget is rendered
    By rendering the Dummy last we get perfect layout logic, but the Dummy is rendered last and is considered first for things like clicks and hover events. Rendering the Dummy first means usages of ImGui.SameLine won't work, but the custom content is rendered on top and will behave correctly. Updated the demo to remove the broken SameLine case.

--- ImGuiStyler/Alignment.cs
+++ ImGuiStyler/Alignment.cs
@@ -9,12 +9,20 @@ public static class Alignment
 
 	public static void CenterWithin(Vector2 contentSize, Vector2 containerSize, Action contentDrawDelegate)
 	{
+		// NOTE: This implementation does NOT work with ImGui.SameLine().
+		// ImGui.Dummy() will update CursorPos and CursorPosPrevLine correctly,
+		// but anything that happens in contentDrawDelegate will overwrite that.
+		// Putting ImGui.Dummy() at the end of this function will work, but that
+		// will cause the Dummy() component to render over the content and capture
+		// clicks, hovers etc. As ImGui.NET doesn't expose imgui_internal.h
+		// there isn't much we can do to solve this
 		var cursorStartPos = ImGui.GetCursorScreenPos();
+		ImGui.Dummy(containerSize);
+		var cursorEndPos = ImGui.GetCursorScreenPos();
 		var clippedsize = new Vector2(Math.Min(contentSize.X, containerSize.X), Math.Min(contentSize.Y, containerSize.Y));
 		var ofset = (containerSize - clippedsize) / 2;
 		ImGui.SetCursorScreenPos(cursorStartPos + ofset);
 		contentDrawDelegate();
-		ImGui.SetCursorScreenPos(cursorStartPos);
-		ImGui.Dummy(containerSize);
+		ImGui.SetCursorScreenPos(cursorEndPos);
 	}
 }
