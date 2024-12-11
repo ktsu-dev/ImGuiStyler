f1a24c8224dad97e53dc76f436ce8d8c6b4b61c8
Matthew Edmondson <matthew.edmondson@gmail.com>
2024-12-11 14:36:28 +1100

    Add XML documentation to Indent class and its members
    The changes introduce XML documentation comments to the `Indent` class and its members. These comments provide descriptions for the class and its methods, explaining their purpose and usage.
    
    - Added summary comment to the `Indent` class.
    - Documented `ByDefault` method with a summary comment.
    - Documented `By` method with a summary comment and parameter description.
    - Added summary comments to `ScopedIndent` class and its constructor.
    - Added summary comments to `ScopedIndentBy` class and its constructor, including parameter description.

--- ImGuiStyler/Indent.cs
+++ ImGuiStyler/Indent.cs
@@ -3,18 +3,44 @@ namespace ktsu.ImGuiStyler;
 using ImGuiNET;
 using ktsu.ScopedAction;
 
+/// <summary>
+/// Provides methods to create scoped indents for ImGui.
+/// </summary>
 public static class Indent
 {
+	/// <summary>
+	/// Creates a scoped indent with the default width.
+	/// </summary>
+	/// <returns>A new instance of <see cref="ScopedIndent"/>.</returns>
 	public static ScopedIndent ByDefault() => new();
+
+	/// <summary>
+	/// Creates a scoped indent with a specified width.
+	/// </summary>
+	/// <param name="width">The width of the indent.</param>
+	/// <returns>A new instance of <see cref="ScopedIndentBy"/>.</returns>
 	public static ScopedIndentBy By(float width) => new(width);
 
+	/// <summary>
+	/// Represents a scoped indent action.
+	/// </summary>
 	public class ScopedIndent : ScopedAction
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ScopedIndent"/> class.
+		/// </summary>
 		public ScopedIndent() : base(ImGui.Indent, ImGui.Unindent) { }
 	}
 
+	/// <summary>
+	/// Represents a scoped indent action with a specified width.
+	/// </summary>
 	public class ScopedIndentBy : ScopedStyleVar
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ScopedIndentBy"/> class with a specified width.
+		/// </summary>
+		/// <param name="width">The width of the indent.</param>
 		public ScopedIndentBy(float width) : base(ImGuiStyleVar.IndentSpacing, width)
 		{
 			ImGui.Indent(width);
