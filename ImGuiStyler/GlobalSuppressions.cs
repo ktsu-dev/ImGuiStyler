63ae241b87a23096377f20ebc80581cd0c967a23
Matthew Edmondson <matthew.edmondson@gmail.com>
2024-12-11 14:35:31 +1100

    Remove GlobalSuppressions.cs file
    Deleted GlobalSuppressions.cs, which contained project-level suppressions for style rules IDE0046 and IDE0045. These suppressions were specific to members of the ktsu.ImGuiStyler.Color class.

--- ImGuiStyler/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.ImGuiStyler.Color.HueToRGB(System.Single,System.Single,System.Single)~System.Single")]
-[assembly: SuppressMessage("Style", "IDE0045:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.ImGuiStyler.Color.ToHSLA(ImGuiNET.ImColor)~System.Numerics.Vector4")]
