794c1dfa3ab4e94763e40987f6ffd85d001b1829
Matthew Edmondson <matthew.edmondson@gmail.com>
2024-12-11 14:37:02 +1100

    Add ScopedStyleVar class for scoped ImGui style management
    Introduce ScopedStyleVar class in ktsu.ImGuiStyler namespace to
    manage ImGui style variables in a scoped manner. The class ensures
    style variables are properly popped when disposed, preventing
    style state issues. Two constructors are provided for initializing
    with Vector2 and float values, each setting up an OnClose action
    to pop the style variable at the end of the scope.

--- ImGuiStyler/ScopedStyleVar.cs
+++ ImGuiStyler/ScopedStyleVar.cs
@@ -4,14 +4,27 @@ using System.Numerics;
 using ImGuiNET;
 using ktsu.ScopedAction;
 
+/// <summary>
+/// Represents a scoped style variable for ImGui. This class ensures that the style variable is popped when disposed.
+/// </summary>
 public class ScopedStyleVar : ScopedAction
 {
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ScopedStyleVar"/> class with a vector value.
+	/// </summary>
+	/// <param name="target">The style variable to be pushed.</param>
+	/// <param name="vector">The vector value to be applied to the style variable.</param>
 	public ScopedStyleVar(ImGuiStyleVar target, Vector2 vector)
 	{
 		ImGui.PushStyleVar(target, vector);
 		OnClose = ImGui.PopStyleVar;
 	}
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ScopedStyleVar"/> class with a scalar value.
+	/// </summary>
+	/// <param name="target">The style variable to be pushed.</param>
+	/// <param name="scalar">The scalar value to be applied to the style variable.</param>
 	public ScopedStyleVar(ImGuiStyleVar target, float scalar)
 	{
 		ImGui.PushStyleVar(target, scalar);
