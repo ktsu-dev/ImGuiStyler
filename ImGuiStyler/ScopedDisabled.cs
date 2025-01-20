f9d4c082274307c45b17c63c7312d0b993aecdfe
Damon Lewis <damon@thethreethousands.com>
2025-01-21 10:28:57 +1100

    Review Feedback
    Remove attempts to track multiple active ScopedDisabled usages. While ScopedDisabled(true) will override ScopedDisabled(false) before the initial ScopeDisabled has been disposed, it won't actually cause a problem aside from maybe not doing what was intended (and that is really a logical flow error anyway)

--- ImGuiStyler/ScopedDisabled.cs
+++ ImGuiStyler/ScopedDisabled.cs
@@ -4,27 +4,19 @@ using ImGuiNET;
 using ktsu.ScopedAction;
 
 /// <summary>
-/// Represents a scoped disabled action which will set ImGui elements as disabled until the class is disposed.
+/// Represents a scoped disabled action which will set Dear ImGui elements as functionally and visually disabled until
+/// the class is disposed.
 /// </summary>
 public class ScopedDisabled : ScopedAction
 {
-	// As per the ImGui documentation: Those can be nested but it cannot be
-	// used to enable an already disabled section (a single BeginDisabled(true)
-	// in the stack is enough to keep everything disabled)
-	private static bool EnabledScopedDisabledActive { get; set; }
-
 	/// <summary>
-	/// Initializes a new instance of the <see cref="ScopedDisabled"/> class.
+	/// Note as per the Dear ImGui documentation: "Those can be nested but it cannot
+	/// be used to enable an already disabled section (a single BeginDisabled(true)
+	/// in the stack is enough to keep everything disabled)"
 	/// </summary>
 	/// <param name="enabled">Should the elements within the scope be disabled</param>
 	public ScopedDisabled(bool enabled)
 	{
-		if (EnabledScopedDisabledActive && !enabled)
-		{
-			throw new ArgumentException("Cannot enable an already disabled section");
-		}
-
-		EnabledScopedDisabledActive = enabled;
 		ImGui.BeginDisabled(enabled);
 		OnClose = ImGui.EndDisabled;
 	}
